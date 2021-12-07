2810bc21d00221731fcbe8f16dc8fa241b2a3b8f
sachidimlaka <sachinisiriwardhana07@gmail.com>
2021-12-07 15:05:45 +0530

    Add wall image to home page

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HeaderComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,16 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+    NgbModule,
   ],
+  exports: [ HomeComponent],
   providers: [],
   bootstrap: [AppComponent]
 })

fa09697fa9576b53af15eab0619078b01a352975
sachidimlaka <sachinisiriwardhana07@gmail.com>
2021-12-22 16:37:50 +0530

    homw componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { CrudComponent } from './crud/crud.component';
+import { AppRoutes } from './app.routing';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CrudComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(AppRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
